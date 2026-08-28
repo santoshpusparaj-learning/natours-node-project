c4cf4d43665a1ff8c7aeaccebbdd6ea95c4911f5
Santosh Pusparaj <ccev4295@nus.edu.sg>
2026-08-28 15:59:39 +0800

    Added the new components

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 import { configDotenv } from "dotenv";
 import { app} from "./app.js"
+import mongoose, {Mongoose} from "mongoose"
 
 configDotenv({
     path: "./config.env"
 })
+
+const DB = process.env.DATABASE_CONNECTION.replace("<db_password>", process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB).then(() => {
+  console.log("DB Connection is successful !!")
+}).catch((err) => console.log(`Error while connecting to Database: ${err}`))
+
+const TourSchema = mongoose.Schema({
+  name: {
+  }
+})
+
 
 const PORT = process.env.PORT || 3000;
 
