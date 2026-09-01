23f82f746ff9c9cef12997acc4acb9521d4a383f
Santosh Pusparaj <santoshpusparaj.learning@gmail.com>
2026-09-02 06:32:43 +0800

    Updated

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,21 +1,43 @@
-import { Tour } from "../models/tourModel";
+import { Tour } from "../models/tourModel.js";
 
-export const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
+export const getAllTours = async (req, res) => {
+  try{
+    const tours = await Tour.find()
+    res.status(200).json({
+      "status" : "success",
+      data: {
+        tours
+      }
+    })
+  } catch(err) {
+    res.status(400).json({
+      "status" : "failed",
+      "error" : {
+        err
+      }
+    })
+  }
 };
 
-export const getTour = (req, res) => {
-  const id = req.params.id * 1;
+export const getTour = async (req, res) => {
+  try{
+    const tour = await Tour.findById(req.params.id)
 
-  res.status(200).json({
-    status: "Success",
-  });
+    res.status(200).json({
+      "status" : "success",
+      data : {
+        tour
+      }
+    })
+  } catch(err){
+    res.status(400).json({
+      "status" : "failed",
+      error : {
+        err
+      }
+    })
+  }
+  
 };
 
 export const createTour = async (req, res) => {
@@ -35,10 +57,28 @@ export const createTour = async (req, res) => {
   }
 };
 
-export const updateTour = (req, res) => {
-  res.status(201).json({
-    status: "Success",
-  });
+export const updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    })
+
+    res.status(200).json({
+      "status" : "success",
+      "data" : {
+        tour
+      }
+    })
+  }
+  catch(err) {
+    res.status(400).json({
+      "status" : "failed",
+      "error" : {
+        err
+      }
+    })
+  }
 };
 
 export const deleteTour = (req, res) => {
@@ -51,15 +91,4 @@ export const deleteAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
   });
-};
-
-//Utility function that will check whether the ID will be valid
-export const checkId = (req, res, next, val) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-  next();
 };
